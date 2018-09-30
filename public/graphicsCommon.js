1b4ef02b9611d2dcc95c198822474b770052572d
arthurivan <arthurivancowdery@gmail.com>
2018-09-30 12:54:09 +0300

    organized js into folders

--- public/graphicsCommon.js
+++
@@ -1,8 +0,0 @@
-function drawBitmapCenteredWithRotation(graphic, atX, atY, width,height, withAngle) {
-	cx.save();
-	cx.translate(atX, atY);
-	cx.rotate(withAngle);
-	cx.drawImage(graphic, -(width/2), -(height/2), carW, carH);
-	cx.restore();
-	
-}
